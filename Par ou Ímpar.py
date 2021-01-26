a5befb8020bcdbe9a28daab5c8576c259c59b439
Rafael Sztajn <78048635+RafaelSztajn@users.noreply.github.com>
2021-01-27 02:15:02 +0300

    Update Par ou Ímpar.py

--- Par ou Ímpar.py
+++ Par ou Ímpar.py
@@ -1,8 +1,7 @@
-print('bem vindo')
-pi=int(input('entre com o seu numero '))
-inpar = pi/1==pi
-par = pi//2==pi/2
-if par!=inpar:
-    print('o numero escolhido é inpar')
-else:
-    print('o numero escolhido é par')
+num = 0
+while True:
+    num = int(input('Entre com o numero'))
+    if num < 0:
+        break
+    for c in range(1, 11):
+        print('{} x {} = {}'.format(num, c, num * c))
